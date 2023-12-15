cc0df485f76c8dafc2caf3cf2cc5a665f7786ccc
Shyam Prasad <shyam.p@thinkpalm.com>
2023-12-15 10:51:32 +0530

    Added necessary files

---
+++ http_endpoint.py
@@ -0,0 +1,108 @@
+import sys
+import json
+from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
+import logging
+sys.path.append(r'D:/backup/PRO/')
+from lib import engine
+import os
+
+# Setting logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Setting mutex lock
+os.environ['state'] = 'IDLE'
+
+class SimpleRequestHandler(BaseHTTPRequestHandler):
+    result = None  # Shared result variable
+    stop_event_flag = None  # Shared stop_event_flag variable
+
+    def do_POST(self):
+        # Get the length of the incoming data
+        content_length = int(self.headers['Content-Length'])
+
+        # Read the incoming data
+        post_data = self.rfile.read(content_length)
+
+        # Decode the JSON data
+        data = json.loads(post_data.decode('utf-8'))
+
+        # Check if the request path matches the desired endpoint for starting or stopping
+        if self.path == '/start_trigger':
+            if os.environ['state'] == 'LOCKED':
+                logger.info("Trigger in locked state...")
+                logger.info("Dropping the trigger")
+                # Send a response
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+
+                response_message = "Triggered started already please wait for completeion"
+                self.wfile.write(json.dumps({'message': response_message}).encode('utf-8'))
+            else:               
+                # Include the custom response message directly in the response body
+                logger.info("Trigger lock aquired")
+                os.environ['state'] = 'LOCKED'
+                response_message = "Triggered Successfully"
+                self.wfile.write(json.dumps({'message': response_message}).encode('utf-8'))
+                # Invoke your specific function to start the engine
+                engine_thread, stop_event_flag = engine.start_engin()
+
+                # Set the shared variables
+                SimpleRequestHandler.result = engine_thread
+                SimpleRequestHandler.stop_event_flag = stop_event_flag
+
+                # Send a response
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+
+                # Include the custom response message directly in the response body
+                response_message = "Triggered Successfully"
+                self.wfile.write(json.dumps({'message': response_message}).encode('utf-8'))
+
+        elif self.path == '/stop_trigger':
+            if SimpleRequestHandler.result is None or SimpleRequestHandler.stop_event_flag is None:
+                # Send a 400 response indicating that do_POST_STOP is invoked before do_POST
+                self.send_response(400)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+
+                # Include the custom response message directly in the response body
+                response_message = "Cleanup any running instance & Clearing buffer"
+                self.wfile.write(json.dumps({'message': response_message}).encode('utf-8'))
+                
+                # Raise an exception
+                raise ValueError("No Trigger Detected...")
+            else:
+
+                # Invoke your specific function to stop the engine
+                engine.stop_engin(SimpleRequestHandler.result, SimpleRequestHandler.stop_event_flag)
+
+                # Send a response
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+
+                # Include the custom response message directly in the response body
+                response_message = "Stopped Successfully"
+                self.wfile.write(json.dumps({'message': response_message}).encode('utf-8'))
+                SimpleRequestHandler.result = None
+                SimpleRequestHandler.stop_event_flag = None
+
+        else:
+            # Send a 404 response for unknown paths
+            self.send_response(404)
+            self.end_headers()
+
+class ThreadedHTTPServer(ThreadingHTTPServer):
+    pass
+
+def run(server_class=ThreadedHTTPServer, handler_class=SimpleRequestHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting server on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
---
+++ inventory.py
@@ -0,0 +1,159 @@
+import sys
+import re
+sys.path.append(r'D:/backup/PRO/')
+from  lib.initiate_ssh_connection import SSHClient
+import mysql.connector
+import socket
+import json
+import threading
+import time
+import requests
+import os
+import logging
+
+# Setting logger 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Interface to be igonred
+ignore_interface = ['cni-podman0', 'fptun0',  'lo',  'veth', 'vneth', 'vnet','io','cni-podman0', 'eth-int', 'vethb', 'vnet', 'vnet','cni-podman1','docker0']
+
+# Setting SQL Connect 
+def sql_connect():
+    host = "192.168.109.137"
+    port = 3306
+    user = "root"
+    password = "password"
+    database = "DEVICE_TRACKER"
+
+    # Establish a connection to the database
+    connection = mysql.connector.connect(
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+        database=database
+    )
+    return connection
+# Function for retreving data from mysql database
+def get_dev_details():
+    dev = {}
+    connection = sql_connect()
+    try:
+    # Create a cursor to execute SQL queries
+        cursor = connection.cursor()
+
+        # Define the SQL query to retrieve IP and TAG from DEVICES table
+        query = r"SELECT ip, tags, ip2, ip3 FROM field"
+
+        # Execute the query
+        cursor.execute(query)
+
+        # Fetch all the rows
+        rows = cursor.fetchall()
+
+        # Print the results
+        for row in rows:
+            ip, tag, ip2, ip3 = row
+            dev[tag] = {ip, ip2, ip3}
+
+    except:
+        logger.critical("Failed to featch database")
+        os.environ['state'] = 'IDLE'
+        logger.info("Released Trigger Lock")
+  #  cursor.close()
+    connection.close()
+    return dev
+# Function for retriving username and password of spcific device
+def get_pass_details(ip):
+    connection = sql_connect()
+    cursor = connection.cursor()
+    query =  r"SELECT username, password FROM field WHERE ip = %s OR ip2 = %s OR ip3 = %s"
+    cursor.execute(query, (ip,ip,ip))
+    cred = cursor.fetchone()
+    connection.close()
+    return cred
+
+# Function for getting ping status
+def check_ping(devices):
+    status_info = {}
+    ssh_list  = []
+    for tag, msip in devices.items():
+        ping_status = {}
+        for ip in msip:
+            try:
+            # Create a socket object and attempt to connect to the IP address on port 80
+                sock = socket.create_connection((ip, 22), timeout=2)
+                sock.close()
+                ping_status[ip] = 'UP'
+            except(socket.timeout, socket.error):
+                ping_status[ip] = 'DOWN'
+        status_info[tag] = ping_status
+    for tag,dev_list in status_info.items():
+        for ip,status in dev_list.items():
+            if status=='UP':
+                ssh_list.append(ip)
+                break
+    return status_info, ssh_list
+
+# Removing unwante interface
+def procees_interface(inf):
+    final_inf = []
+    try:
+        inf.remove('')
+    except ValueError:
+        pass
+
+    for i in inf:
+        if i[-1] == ':':
+            final_inf.append(i[:-1])
+        else:
+            final_inf.append(i)
+
+    final_inf = [interface for interface in final_inf if not any(ignore in interface for ignore in ignore_interface)]
+
+    return final_inf
+
+# Get tag based on the interface
+def get_tag(ip):
+    sql_query = "SELECT tags FROM field WHERE ip = %s OR ip2 = %s OR ip3 = %s;"
+    connection = sql_connect()  # Make sure to implement your sql_connect function
+    cursor = connection.cursor()
+    cursor.execute(sql_query, (ip, ip, ip))
+    tags = cursor.fetchall()
+    return tags
+    
+# Function of interface status
+def initiate_ssh(l):
+    status_check = {}
+    layer_status = {}
+    for ip in l:
+            ifconfig_status = ''
+            route_status = ''
+            arp_status = ''
+            status = {}
+            l_status = {}
+            username, password = get_pass_details(ip)
+            obj = SSHClient(ip,username,password)
+            obj.connect()
+            try:
+                interface = str(obj.run_execmcd("ifconfig  | awk '/^[a-zA-Z]/{print $1}'"))
+                interface = interface.split('\n')        
+                interface = procees_interface(interface)
+                ifconfig_status = obj.run_execmcd('ifconfig')
+                route_status = obj.run_execmcd('route -n')
+                arp_status = obj.run_execmcd('arp -a')
+                for i in interface:
+                    link = obj.run_execmcd(f"ethtool {i} | grep 'Link detected' | awk '{{print $3}}'")
+                    status[i] = link.strip()
+                tag = get_tag(ip)
+                tag = tag[0][0]
+                status_check[tag] = status
+                l_status['ifconfig'] = ifconfig_status
+                l_status['route'] = route_status
+                l_status['arp'] = arp_status
+                layer_status[tag] = l_status
+                obj.close_connection()
+            except:
+                logger.critical("Issue facing while running ethtool") # Failed to SSH
+    return json.dumps(status_check), json.dumps(layer_status)
